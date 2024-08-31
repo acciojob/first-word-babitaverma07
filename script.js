797bb6425f23941206a6c626d9a930b274c75d54
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 23:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Check if the string is empty
+    if (str === '') {
+        return '';
+    }
+    
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    // Return the substring up to the first space
+    return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// Example usage:
+console.log(firstWord('see and stop')); // 'see'
+console.log(firstWord(' Hello World!')); // 'Hello'
+console.log(firstWord('12345')); // '12345'
+console.log(firstWord('')); // ''
